45b2fd96690c3a010386e98805b1951131383974
Greg Morgan <gregm9999@gmail.com>
2019-03-20 21:30:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase()) {
     return "I can't hear you!";
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
   }
 }
